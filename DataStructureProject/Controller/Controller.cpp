b7837f8f8913c0c213226ae99c2d2b6453a0f42a
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-02 11:49:35 -0700

    Called the findMaxAndMin() method in the start() method. (File wasn't saved on previous commit).

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -12,19 +12,7 @@ using namespace std;
 
 void Controller :: start()
 {
-    Timer codeTimer;
-    codeTimer.startTimer();
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    codeTimer.resetTimer();
-    codeTimer.startTimer();
-    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/bcha1422/Documents/C++ Projects/DataStructureProject/DataStructureProject/Data/crime.csv");
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    for (int i = 200; i <= 215; i++)
-    {
-        cout << i << " is " << myData[i] << endl;
-    }
+    findMaxAndMin();
 }
 
 void Controller :: findMaxAndMin()
