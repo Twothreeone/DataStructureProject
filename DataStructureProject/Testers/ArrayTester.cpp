4db0ae4f9c71b763d3493909c6c82faec1adb0d0
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-06 12:20:44 -0700

    Added implementation for the testArrayUse() method.

--- DataStructureProject/Testers/ArrayTester.cpp
+++ DataStructureProject/Testers/ArrayTester.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "ArrayTester.hpp"
+
+void ArrayTester :: testArrayUse()
+{
+    Array<int> firstArray(10);
+    for (int i = 0; i < 10; i++)
+    {
+        firstArray.setAtIndex(i, i);
+    }
+    for (int i = 0; i < firstArray.getSize(); i++)
+    {
+        cout << firstArray[i] << endl;
+    }
+    Array<int> secondArray(400);
+    cout << secondArray.getSize() << endl;
+    cout << firstArray.getSize() << endl;
+    Array<int> thirdArray(secondArray);
+    cout << thirdArray.getSize() << endl;
+    firstArray = thirdArray;
+    cout << firstArray[4] << endl;
+    thirdArray[5] = 123;
+    cout << thirdArray[5] << endl;
+}
