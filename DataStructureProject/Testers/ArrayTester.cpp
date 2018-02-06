e7a96c031f50a80ec24fa43e852e4b849d32f3c5
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-06 12:06:58 -0700

    Added an ArrayTester file and added to its header file.

---
+++ DataStructureProject/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructureProject
+//
+//  Created by Charlesworth, Benjamin on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
