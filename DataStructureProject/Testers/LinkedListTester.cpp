80b0fe69ad4d0c210f4c271baa120d0111adf6db
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-02-14 13:00:28 -0700

    Added the last method needed in the LinkedListTester.

--- DataStructureProject/Testers/LinkedListTester.cpp
+++ DataStructureProject/Testers/LinkedListTester.cpp
@@ -34,3 +34,26 @@ void LinkedListTester :: testListBasics()
     cout << numbers.getFront()->getData() << " is at the front of the list and should be 2312" << endl;
     cout << numbers.getEnd()->getData() << " is at the end of the list and should be 32567" << endl;
 }
+
+void LinkedListTester :: testListWithData()
+{
+    Timer listTimer;
+    
+    listTimer.startTimer();
+    LinkedList<CrimeData> crimes = FileController :: readDataToList("/Users/bwor0809/Documents/C++/DataStructureProject/DataStructureProject/Data/crime.csv");
+    
+    listTimer.stopTimer();
+    cout << "This is how long it took to read the structure into our custom data structure" << endl;
+    listTimer.displayInformation();
+    
+    listTimer.resetTimer();
+    cout << "Here is how long it takes to access a random data value" << endl;
+    listTimer.startTimer();
+    int randomLocation = (rand() * rand()) % crimes.getSize();
+    cout << "The random index is: " << randomLocation << endl;
+    double totalViolentRate = crimes.getFromIndex(randomLocation).getAllViolentRates();
+    listTimer.stopTimer();
+    cout << "The random crimes stat is: " << totalViolentRate << " , and here is the timer" << endl;
+    listTimer.displayInformation();
+}
+
