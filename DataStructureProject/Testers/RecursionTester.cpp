775093efa6e3f10787c0d1bfeb560cfd3104e9f4
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-05 11:43:31 -0700

    Added a RecursionTester class. Finished the class definition and added method stubs, implementation needed.

---
+++ DataStructureProject/Testers/RecursionTester.cpp
@@ -0,0 +1,29 @@
+//
+//  RecursionTester.cpp
+//  DataStructureProject
+//
+//  Created by Charlesworth, Benjamin on 3/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "RecursionTester.hpp"
+
+int RecursionTester :: fib(int number)
+{
+    return 0;
+}
+
+string RecursionTester :: mystery(string word)
+{
+    return "";
+}
+
+void RecursionTester :: testRecursionNumbers()
+{
+    
+}
+
+void RecursionTester :: testRecursionString()
+{
+    
+}
