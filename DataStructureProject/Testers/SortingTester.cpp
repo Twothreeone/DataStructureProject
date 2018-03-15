00ea20e328bee5b38afce6e0b866f7bee1086e68
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-15 11:52:14 -0600

    Added the SortingTester class and finished its class definition.

---
+++ DataStructureProject/Testers/SortingTester.cpp
@@ -0,0 +1,9 @@
+//
+//  SortingTester.cpp
+//  DataStructureProject
+//
+//  Created by Charlesworth, Benjamin on 3/15/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "SortingTester.hpp"
