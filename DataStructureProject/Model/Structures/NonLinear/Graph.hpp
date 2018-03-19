6b3bbe5e6ae9bd4ae9ca735b8f0cd6251c1efa61
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-19 13:50:14 -0600

    Added the costTraversal method.

--- DataStructureProject/Model/Structures/NonLinear/Graph.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Graph.hpp
@@ -185,11 +185,40 @@ void Graph<Type> :: depthFirstTraversal(Graph<Type> & currentGraph, int vertex, 
     cout << currentGraph[vertex] << ", " << endl;
     for (setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
     {
-        if(!visited[*setIterator])
+        if (!visited[*setIterator])
         {
             depthFirstTraversal(currentGraph, *setIterator, visited);
         }
     }
+}
+
+template <class Type>
+int Graph<Type> :: costTraversal(Graph<Type> & currentGraph, int vertex)
+{
+    assert(vertex < currentGraph.size());
+    int cost = 0;
+    bool visited[MAXIMUM];
+    std::set<int> connections;
+    std::set<int>::iterator setIterator;
+    std::queue<int> vertexQueue;
+    std::fill_n(visited, currentGraph.size(), false);
+    visited[vertex] = true;
+    vertexQueue.push(vertex);
+    while (!vertexQueue.empty())
+    {
+        connections = currentGraph.neighbors(vertexQueue.front());
+        vertexQueue.pop();
+        for (setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+        {
+            if (!visited[*setIterator])
+            {
+                cost += weightCostMatrix[vertex][*setIterator];
+                visited[*setIterator] = true;
+                vertexQueue.push(*setIterator);
+            }
+        }
+    }
+    return cost;
 }
 
 #endif /* Graph_hpp */
