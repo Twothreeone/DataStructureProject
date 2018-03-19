5f94647c82287f7b980eb9c6da48609f31a354cd
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-19 13:54:04 -0600

    Added the breadthFirstTraversal implementation for the Graph class.

--- DataStructureProject/Model/Structures/NonLinear/Graph.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Graph.hpp
@@ -221,4 +221,32 @@ int Graph<Type> :: costTraversal(Graph<Type> & currentGraph, int vertex)
     return cost;
 }
 
+template <class Type>
+void Graph<Type> :: breadthFirstTraversal(Graph<Type> & currentGraph, int vertex)
+{
+    assert(vertex < currentGraph.size());
+    bool visited[MAXIMUM];
+    std::set<int> connections;
+    std::set<int>::iterator setIterator;
+    std::queue<int> vertexQueue;
+    std::fill_n(visited, currentGraph.size(), false);
+    visited[vertex] = true;
+    cout << currentGraph[vertex] << endl;
+    vertexQueue.push(vertex);
+    while (!vertexQueue.empty())
+    {
+        connections = currentGraph.neighbors(vertexQueue.front());
+        vertexQueue.pop();
+        for (setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+        {
+            if (!visited[*setIterator])
+            {
+                visited[*setIterator] = true;
+                cout << currentGraph[*setIterator] << endl;
+                vertexQueue.push(*setIterator);
+            }
+        }
+    }
+}
+
 #endif /* Graph_hpp */
