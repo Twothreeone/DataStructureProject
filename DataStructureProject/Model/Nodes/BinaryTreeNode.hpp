925204c2b5852fe79737a329bfa660e052c72e57
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-27 12:18:55 -0600

    Added a UML for a tree data structure and added a header file for the BinaryTreeNode and finished it.

---
+++ DataStructureProject/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,84 @@
+//
+//  BinaryTreeNode.hpp
+//  DataStructureProject
+//
+//  Created by Charlesworth, Benjamin on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class BinaryTreeNode<Type> : public Node<Type>
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * left;
+    BinaryTreeNode<Type> * right;
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    BinaryTreeNode<Type> * getRoot();
+    BinaryTreeNode<Type> * getLeft();
+    BinaryTreeNode<Type> * getRight();
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setLeftNode(BinaryTreeNode<Type> * left);
+    void setRightNode(BinaryTreeNode<Type> * right);
+};
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeft()
+{
+    return left;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRight()
+{
+    return right;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRootNode(BinaryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftNode(BinaryTreeNode<Type> * left)
+{
+    this->left = left;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightNode(BinaryTreeNode<Type> * right)
+{
+    this->right = right;
+}
+
+#endif /* BinaryTreeNode_hpp */
