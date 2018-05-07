7765ac7d6e909f95a68e38b39349989cb9f44f20
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-05-07 11:45:12 -0600

    Added retreival methods to the Hashtable and implemented the prime methods.

--- DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
@@ -10,6 +10,8 @@
 #define Hashtable_hpp
 
 #include "../Nodes/HashNode.hpp"
+#include <cmath>
+#include <assert.h>
 
 template <class Type>
 class Hashtable
@@ -29,6 +31,8 @@ public:
     ~HashTable();
     void insert(Type data);
     long getSize();
+    HashNode<Type> * get(long index);
+    bool contains(HashNode<Type> * value);
 };
 
 template <class Type>
@@ -49,13 +53,40 @@ Hashtable<Type> :: ~Hashtable()
 template <class Type>
 long Hashtable<Type> :: getNextPrime()
 {
-    return -1;
+    long nextPrime = (this->capacity * 2) + 1;
+    while (!isPrime(nextPrime))
+    {
+        nextPrime += 2;
+    }
+    return nextPrime;
 }
 
 template <class Type>
 bool Hashtable<Type> :: isPrime(long current)
 {
-    return false;
+    if (current <= 1)
+    {
+        return false;
+    }
+    else if (current == 2 || current == 3)
+    {
+        return true;
+    }
+    else if (current % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (int next = 3; next <= sqrt(current) + 1; next += 2)
+        {
+            if (current % next == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 template <class Type>
@@ -136,6 +167,28 @@ void Hashtable<Type> :: insert(Type data)
             internal[updatedPosition] = temp;
         }
     }
+}
+
+template <class Type>
+HashNode<Type> * Hashtable<Type> :: get(long index)
+{
+    assert(index < capacity);
+    return internalStorage[index];
+}
+
+template <class Type>
+bool Hashtable<Type> :: contains(HashNode<Type> * value)
+{
+    if (internalStorage[findPosition(value)]->getData() == value->getData())
+    {
+        return true;
+    }
+    long other = handleCollision(findPosition(value));
+    if (internalStorage[other]->getData() == value->getData())
+    {
+        return true;
+    }
+    return false;
 }
 
 template <class Type>
